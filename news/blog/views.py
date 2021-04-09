83132c5c0df76d12740092cee43bdb319fb04780
karloshrai <karloshrai270@gmail.com>
2021-04-09 15:08:29 +0545

    'update'

--- news/blog/views.py
+++ news/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'pages/index/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about/about.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact/contact.html')
